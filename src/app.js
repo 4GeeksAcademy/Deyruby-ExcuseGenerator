af504355963e9fde36794bc1cee5439c961add0e
Dey <128061593+Deyruby@users.noreply.github.com>
2023-11-16 22:52:01 +0000

    tarea terminada

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function excuses() {
+    let who = ["Deyruby", "He", "My Mother", "My husband"];
+
+    let action = ["did not find", "broke", "lost", "forgot"];
+
+    let what = [
+      "the table",
+      "the computer",
+      "the car",
+      "the phone",
+      "the keys"
+    ];
+
+    let when = ["today", "yesterday", "in the afternoon", "in the morning"];
+
+    let indexwho = Math.floor(Math.random() * who.length);
+    let indexaction = Math.floor(Math.random() * action.length);
+    let indexwhat = Math.floor(Math.random() * what.length);
+    let indexwhen = Math.floor(Math.random() * when.length);
+
+    return (
+      who[indexwho] +
+      " " +
+      action[indexaction] +
+      " " +
+      what[indexwhat] +
+      " " +
+      when[indexwhen]
+    );
+  }
+  console.log(excuses());
 };
